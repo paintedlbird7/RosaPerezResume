5d50280da6d8515fd8c7720d4e519732e167fc31
paintedlbird7 <rosaperezlarios@gmail.com>
2017-08-19 17:37:42 -0700

    Add files via upload

---
+++ code.js
@@ -0,0 +1,31 @@
+console.log ('Rosa Perez');
+var age = 27
+var name = "Rosa Perez";
+var Name = "Rosa";
+var NaMe = "different variable";
+var isRainingToday = "false";
+
+
+var rosaPerez = prompt("what is your first name?");
+console.log("Hi, " + rosaPerez + "!");
+// just like when a page asks for a password, a pop up promts
+
+var dateOfBirth = 26;
+
+var rosaPerez = promt("what is your day of birth?");
+console.log("26");
+
+
+
+
+
+age = 35;
+
+console.log(age);
+console.log(name);
+console.log("Rosa is 27");
+
+window.alert("Hello World!")
+window.alert(1+1)
+window.promt("How are you feeling today?")
+alert("Hello World!")
